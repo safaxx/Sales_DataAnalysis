e86105bf5b4ce6680d1a72725d75f3b6cf769f82
safa <85731578+safaxx@users.noreply.github.com>
2021-10-19 10:19:09 +0530

    Add files via upload

---
+++ Fact_InternetSalesSql.sql
@@ -0,0 +1,33 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+       [ProductKey]
+      ,[OrderDateKey]
+      ,[DueDateKey]
+      ,[ShipDateKey]
+      ,[CustomerKey]
+      --,[PromotionKey]
+      --,[CurrencyKey]
+      --,[SalesTerritoryKey]
+      ,[SalesOrderNumber]
+      --,[SalesOrderLineNumber]
+      --,[RevisionNumber]
+      --,[OrderQuantity]
+      --,[UnitPrice]
+      --,[ExtendedAmount]
+      --,[UnitPriceDiscountPct]
+      --,[DiscountAmount]
+      --,[ProductStandardCost]
+      --,[TotalProductCost]
+      ,[SalesAmount]
+      --,[TaxAmt]
+      --,[Freight]
+      --,[CarrierTrackingNumber]
+      --,[CustomerPONumber]
+      --,[OrderDate]
+      --,[DueDate]
+      --,[ShipDate]
+  FROM 
+    [AdventureWorksDW2019].[dbo].[FactInternetSales]
+  WHERE 
+    LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2  -- AS THE BUSINESS RPOBLEM SPECIFIED FOR LAST 2 YEAR ANALYSIS 
+ ORDER BY OrderDateKey
